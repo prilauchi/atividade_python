0040f7d8606f9c113718cadb9b8af2f20bedfd91
Priscila Oliveira <prilauchiinfinity@gmail.com>
2024-08-23 17:42:11 -0300

    criado arquivo atividade

---
+++ atividade.py
@@ -0,0 +1,8 @@
+pergunta1 = input("Telefonou para a vítima")
+pergunta2 = input("Esteve no local do crime")
+pergunta3 = input("Mora perto da vítima")
+pergunta4 = input("Devia para a vítima")
+pergunta5 = input("Já trabalhou com a vítima")
+
+
+
